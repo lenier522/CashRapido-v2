441382b4abffa14e86adc4bcfe00c5c80ea90468
lenier522 <leniercruz02@nauta.cu>
2026-02-24 11:37:59 -0500

    cambio de version

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -411,7 +411,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 showLicensePage(
                   context: context,
                   applicationName: context.t('app_name'),
-                  applicationVersion: '1.11.6',
+                  applicationVersion: '1.12.1',
                   applicationIcon: Icon(
                     Icons.account_balance_wallet,
                     size: 48,
@@ -451,7 +451,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 showAboutDialog(
                   context: context,
                   applicationName: context.t('app_name'),
-                  applicationVersion: '1.11.6',
+                  applicationVersion: '1.12.1',
                   applicationIcon: Icon(
                     Icons.account_balance_wallet,
                     size: 48,
@@ -829,7 +829,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
       child: Column(
         children: [
           Text(
-            'CashRapido v1.11.6',
+            'CashRapido v1.12.1',
             style: GoogleFonts.outfit(
               color: Theme.of(context).disabledColor,
               fontSize: 12,
