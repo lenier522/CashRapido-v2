6168b3d906c922e53c98c23fa671c3eea9a36000
lenier522 <leniercruz02@nauta.cu>
2026-08-17 18:44:30 -0400

    chore: bump version to 1.23.51 and disable test payment method

--- lib/providers/app_provider.dart
+++ lib/providers/app_provider.dart
@@ -244,8 +244,8 @@ class AppProvider with ChangeNotifier {
           id: 'test_cuba',
           name: 'Prueba (Test)',
           iconAsset: 'assets/icons/test.png',
-          isEnabled: true,
-          isVisible: true,
+          isEnabled: false,
+          isVisible: false,
           isTest: true,
         ),
         PaymentMethod(
